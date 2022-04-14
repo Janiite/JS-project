9464262a494c9b53799933b6d741d164b7e9adce
Jana Fedotovska <69121010+Janiite@users.noreply.github.com>
2022-04-14 22:06:54 +0300

    commit

---
+++ backend copy/index.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  key: "userID",
+  secret: "test",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 60*60*2,
+  },
+
+
+}))
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "test",
+});
+
+app.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log(err);
+    }
+    db.query(
+      "INSERT INTO users (username, password) VALUES (?,?)",
+      [username, hash],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send("Values Inserted");
+        }
+      }
+    );
+  });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  db.query(
+    "SELECT * FROM users WHERE username = ? ",
+    [username],
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      }
+      if (result.length > 0) {
+        bcrypt.compare(password, result[0].password, (error, response) => {
+          if (response) {
+            req.session.user = result;
+            console.log(req.session.user);
+            res.send(result);
+          } else {
+            res.send({ message: "Wrong password" });
+          }
+        });
+      } else {
+        res.send({ message: "User not found" });
+      }
+    }
+  );
+});
+
+// app.post("/check", (req, res) => {
+//   db.query(
+//     "SELECT * FROM users WHERE username = ?"[username],
+//     (err, result) => {
+//       if (err) {
+//         res.send({ err: err });
+//       }
+//       if (result.length > 0) {
+//         res.send({ message: "User exists" });
+//       } else {
+//         res.send(result);
+//       }
+//     }
+//   );
+// });
+
+app.post("/update", (req, res) => {
+  const id = req.body.id;
+  const wage = req.body.wage;
+  db.query("UPDATE users SET  = ? WHERE id = ?", [], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM users WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Your server is running on port 3001");
+});
